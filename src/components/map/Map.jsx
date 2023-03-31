00997b67b0b938252da98990cf35ef17834a7ebb
Eliza Thomas <elizamthomas1994@gmail.com>
2023-03-31 13:32:09 +0100

    Added current location marker to google maps

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Marker, Map, GoogleApiWrapper } from 'google-maps-react';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+export class MapContainer extends React.Component {
+  state = { userLocation: { lat: 32, lng: 32 }, loading: true };
 
-class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 51.515968,
-         lng: -0.174970
-        }}
-      />
+  componentDidMount(props) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        this.setState({
+          userLocation: { lat: latitude, lng: longitude },
+          loading: false
+        });
+      },
+      () => {
+        this.setState({ loading: false });
+      }
     );
+  }
+
+  render() {
+    const { loading, userLocation } = this.state;
+    const { google } = this.props;
+
+    if (loading) {
+      return null;
+    }
+
+    return (
+      <Map google={google} initialCenter={userLocation} zoom={18}>
+        <Marker position={userLocation} />
+      </Map>
+    )
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyC2cVIa--afAFbfhAciJaVhWfCMi7MXXWw'
+  apiKey: "AIzaSyC2cVIa--afAFbfhAciJaVhWfCMi7MXXWw"
 })(MapContainer);
