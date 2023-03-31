29b2ec0081be38f3f07f082a2aec4eb64404f297
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-03-31 15:22:11 +0100

    fix linting

--- src/components/navbar/FloatingActionButtons.jsx
+++ src/components/navbar/FloatingActionButtons.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
 import HomeIcon from '@mui/icons-material/Home';
