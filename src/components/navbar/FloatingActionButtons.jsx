fe5ff07999a8ce7f440a9e5617a3a3748ea69079
Tian <Tian.pan2003@gmail.com>
2023-03-31 14:54:36 +0100

    added icons

--- src/components/navbar/FloatingActionButtons.jsx
+++ src/components/navbar/FloatingActionButtons.jsx
@@ -5,18 +5,27 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import HomeIcon from '@mui/icons-material/Home';
+import WorkIcon from '@mui/icons-material/Work';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 
 export default function FloatingActionButtons() {
     return (
       <Box sx={{ '& > :not(style)': { m: 1 } }}>
+        <Fab color="primary" aria-label="home">
+          <HomeIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="edit">
+          <WorkIcon />
+        </Fab>
         <Fab color="primary" aria-label="add">
           <AddIcon />
         </Fab>
         <Fab color="secondary" aria-label="edit">
-          <EditIcon />
+          <PersonPinIcon />
         </Fab>
         <Fab variant="extended">
-          <NavigationIcon sx={{ mr: 1, zIndex: 'bottom'}} />
+          <NavigationIcon sx={{ mr: 1 }} />
           Navigate
         </Fab>
         <Fab disabled aria-label="like">
